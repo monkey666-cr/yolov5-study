a91b54e92fbe1dd38d1100f4be7ed0a9018f5428
chenrun <17610780919@163.com>
2023-06-05 22:35:49 +0800

    目标检测

--- src/runtime.cu
+++ src/runtime.cu
@@ -70,38 +70,78 @@ int main(int argc, char **argv)
     // ====== 4. 创建输入输出缓冲区 ======
     samplesCommon::BufferManager buffers(mEngine);
 
+    auto cap = cv::VideoCapture(input_video_path);
+
+    int width = int(cap.get(cv::CAP_PROP_FRAME_WIDTH));
+    int height = int(cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+    int fps = int(cap.get(cv::CAP_PROP_FPS));
+
+    // 写入MP4文件，参数分别是：文件名，编码格式，帧率，帧大小
+    cv::VideoWriter writer("./output/record.mp4", cv::VideoWriter::fourcc('H', '2', '6', '4'), fps, cv::Size(width, height));
+
     cv::Mat frame;
+    int frame_index{0};
 
     // 申请GPU内存
+    cuda_preprocess_init(width * height);
 
-    // ====== 5. 执行推理 ======
-    context->executeV2(buffers.getDeviceBindings().data());
-    // 拷贝回host
-    buffers.copyOutputToHost();
-
-    // 从buffer manager中获取模型输出
-    int32_t *num_det = (int32_t *)buffers.getHostBuffer(kOutNumDet);
-    int32_t *cls = (int32_t *)buffers.getHostBuffer(kOutDetCls);
-    float *conf = (float *)buffers.getHostBuffer(kOutDetScores);
-    float *bbox = (float *)buffers.getHostBuffer(kOutDetBBoxes);
-
-    std::vector<Detection> bboxs;
-    yolo_nms(bboxs, num_det, cls, conf, bbox, kConfThresh, kNmsThresh);
-    
-
-    // 遍历检测结果
-    for (size_t j = 0; j < bboxs.size(); j++)
+    while (cap.isOpened())
     {
-        cv::Rect r = get_rect(frame, bboxs[j].bbox);
-        cv::rectangle(frame, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
-        cv::putText(frame, std::to_string((int)bboxs[j].class_id), cv::Point(r.x, r.y - 10), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0x27, 0xC1, 0x36), 2);
-    }
+        // 统计运行时间
+        auto start = std::chrono::high_resolution_clock::now();
+        cap >> frame;
+        if (frame.empty())
+        {
+            std::cout << "文件处理完毕" << std::endl;
+            break;
+        }
+        frame_index++;
+        // 输入预处理
+        process_input(frame, (float *)buffers.getDeviceBuffer(kInputTensorName));
 
-    cv::imshow("frame", frame);
-    // 写入文件
-    cv::imwrite("./result.jpg", frame);
+        // ====== 5. 执行推理 ======
+        context->executeV2(buffers.getDeviceBindings().data());
+
+        // 拷贝回host
+        buffers.copyOutputToHost();
+
+        // 从buffer manager中获取模型输出
+        int32_t *num_det = (int32_t *)buffers.getHostBuffer(kOutNumDet);
+        int32_t *cls = (int32_t *)buffers.getHostBuffer(kOutDetCls);
+        float *conf = (float *)buffers.getHostBuffer(kOutDetScores);
+        float *bbox = (float *)buffers.getHostBuffer(kOutDetBBoxes);
+
+        std::vector<Detection> bboxs;
+        yolo_nms(bboxs, num_det, cls, conf, bbox, kConfThresh, kNmsThresh);
+        
+        // 结束时间
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        auto time_str = std::to_string(elapsed) + "ms";
+        auto fps_str = std::to_string(1000 / elapsed) + "fps";
+
+
+        // 遍历检测结果
+        for (size_t j = 0; j < bboxs.size(); j++)
+        {
+            cv::Rect r = get_rect(frame, bboxs[j].bbox);
+            cv::rectangle(frame, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+            cv::putText(frame, std::to_string((int)bboxs[j].class_id), cv::Point(r.x, r.y - 10), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0x27, 0xC1, 0x36), 2);
+        }
+
+        cv::putText(frame, time_str, cv::Point(50, 50), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+        cv::putText(frame, fps_str, cv::Point(50, 100), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+
+        cv::imshow("frame", frame);
+        // 写入视频文件
+        writer.write(frame);
+        std::cout << "处理完第" << frame_index << "帧" << std::endl;
+        if (cv::waitKey(1) == 27)
+            break;
+    }
     
     // ====== 6. 释放内存 ======
+    cuda_preprocess_destroy();
 
     return 0;
 }
